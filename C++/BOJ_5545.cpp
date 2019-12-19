4131dc3799cd6481ed5452fe70d80366a59f5f93
sckimynwa <tsb03672@naver.com>
2019-12-19 15:55:00 +0900

    BOJ 2752, 2443 c++

---
+++ C++/BOJ_5545.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, dow, top, calDow;
+    cin >> n >> dow >> top >> calDow;
+
+    int calTop[n];
+    for(int i=0;i<n;i++){
+        cin >> calTop[i];
+    }
+
+    return 0;
+}
