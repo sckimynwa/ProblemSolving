6d886a5ca47ddf93ed64442e0fbdc8f2b63d1a58
sckimynwa <tsb03672@naver.com>
2019-12-19 09:59:37 +0900

    BOJ_14645 c++

---
+++ C++/BOJ_10768.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, d;
+    cin >> m >> d;
+    if(m > 2) cout << "After" << endl;
+    else if(m < 2) cout << "Before" << endl;
+    else {
+        if(d > 18) cout << "After" << endl;
+        else if(d <18) cout << "Before" << endl;
+        else cout << "Special" << endl;
+    }
+    return 0;
+}
