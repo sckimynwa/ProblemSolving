ef6265b6281a95f373d7f1bd369044044edce2a3
sckimynwa <tsb03672@naver.com>
2019-12-24 15:04:57 +0900

    c++ ps

---
+++ C++/BOJ_15964.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    
+    long n, m;
+    cin >> n >> m;
+
+    long retval = n*n - m*m;
+    cout << retval << endl;
+
+    return 0;
+}
