2209f7e8528e524191fa3fce105d7915f7f36322
user.email <tsb03672@naver.com>
2020-03-01 20:18:34 +0900

    BOJ_1786.cpp

---
+++ C++/BOJ_5525.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int n, m;
+    cin >> n >> m;
+    string str;
+    getline(cin, str);
+
+    
+
+    return 0;
+}
